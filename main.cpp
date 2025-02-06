a74443c1ed834b0e6b6eac638ac91cb04611e33a
LukaszKucharczyk3 <lukaszkucharczyk123@gmail.com>
2025-02-06 09:44:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "funkcje.cpp"  // Wczytanie wszystkich funkcji i struktur
+#include "funkcje.cpp"  
 
 int main() {
-    // Ustawienie polskich znaków w konsoli (Windows):
+    
     system("chcp 1250 > nul");
 
-    // Sprawdzamy, czy istnieje domyœlne konto pracownicze (admin/admin):
+    
     sprawdzLubStworzDomyslneKontoPracownicze();
 
     int wybor = 0;
@@ -18,7 +18,7 @@ int main() {
                 logowanieUzytkownik();
                 break;
             case 3:
-                // Wyjœcie z programu
+               
                 break;
             default:
                 cout << RED << "Niepoprawna opcja." << RESET << endl;
